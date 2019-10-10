442787970b2d1f45c0473eaef14f52510595290a
meijie <656983975@qq.com>
2019-10-10 16:33:38 +0800

    新建tabbar

--- src/pages/tabBar/index/index.vue
+++ src/pages/tabBar/index/index.vue
@@ -3,14 +3,12 @@
  * @Author: meijie
  * @Date: 2019-09-19 22:10:32
  * @LastEditors: meijie
- * @LastEditTime: 2019-09-25 11:14:18
+ * @LastEditTime: 2019-10-10 16:33:17
  -->
 <template>
-	<view class="content">
-		<image @click="testClick" class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
+	<view class="page index-page">
+		<!--我的简历-->
+		<view class="module"></view>
 	</view>
 </template>
 
@@ -18,47 +16,20 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+
 			}
 		},
 		onLoad() {
-			uni.navigateTo({
-				url: '/pages/login/login'
-			})
+
 		},
 		methods: {
-			testClick () {
-				uni.request({
-					method: 'POST',
-					url: '/resume/api/v1/login',
-					data: {
-						name: 'meijie',
-						password: '1234567',
-					}
-				}).then((res) => {
-					console.log(res)
-				}).catch((err) => {
-					console.log(err)
-				})
-			}
+
 		}
 	}
 </script>
 
 <style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
+	.index-page{
 
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
 	}
 </style>
